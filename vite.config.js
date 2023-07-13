35a85f3e21dcfafb15dd857107a38ebf8668e638
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-07-13 23:51:17 +0300

    add linter

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://Yaroslav-Zozulya.github.io/",
+  base: "https://Yaroslav-Zozulya.github.io/pastry-calculator",
   plugins: [react()],
   resolve: {
     alias: {
