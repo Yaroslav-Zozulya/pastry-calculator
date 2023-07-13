0c4eeb5908ea7ebdf320d4e5ccb4dcdfb121f4a0
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-07-13 23:50:17 +0300

    add linter

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "https://Yaroslav-Zozulya.github.io/",
   plugins: [react()],
   resolve: {
     alias: {
