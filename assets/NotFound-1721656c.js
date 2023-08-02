00443d305ec31374c1024808e14ddae00ec53b32
Yaroslav-Zozulya <91027713+Yaroslav-Zozulya@users.noreply.github.com>
2023-08-02 11:19:33 +0000

    Deploying to gh-pages from @ Yaroslav-Zozulya/pastry-calculator@5ada4330dfe7d59fcd64d5e4c283f7a77f5e82e6 🚀

--- assets/NotFound-1721656c.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-b413c2b0.js";const r=()=>o.jsx("div",{children:"404 NotFound"});export{r as default};
---
+++ assets/NotFound-70e5f920.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-c6fa181a.js";const r=()=>o.jsx("div",{children:"404 NotFound"});export{r as default};
